de1572d8f56f8a9ab008837c9d0370ef0ba8641b
ale2360 <acxel333@gmail.com>
2021-11-28 15:48:12 -0600

    Cambio1

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAWCjnWp3YBjFeUpPsOXKdp0bViI-SSLsE",
+  authDomain: "storagemx-c170e.firebaseapp.com",
+  projectId: "storagemx-c170e",
+  storageBucket: "storagemx-c170e.appspot.com",
+  messagingSenderId: "606049261713",
+  appId: "1:606049261713:web:c375b4008b4c092a7fc730",
+  measurementId: "G-BTFMDRE227"
+});
